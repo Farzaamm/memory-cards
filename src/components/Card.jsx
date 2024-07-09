88310a8c81c5b1247433cef1be4f2ecbef120fc6
Farzaamm <93753345+Farzaamm@users.noreply.github.com>
2024-07-09 10:10:53 +0800

    shuffleArray added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-function Card( { pokemonImg, name } ) {
+function Card( { pokemonImg, name, onClick } ) {
   return (
-    <div className="card">
+    <div className="card" onClick={onClick}>
       <img src={pokemonImg} alt={`image of ${name}`} />
       <h4>{name}</h4>
     </div>
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,48 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import Card from './Card';
 
-function Cards() {
-  const [pokemon, setPokemon] = useState([]);
-  const [pokemonImage, setPokemonImage] = useState([]);
+    function Cards() {
+    const [pokemon, setPokemon] = useState([]);
+    const [pokemonImage, setPokemonImage] = useState([]);
 
-  useEffect(() => {
-    async function fetchPokemon() {
-      const response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20');
-      const data = await response.json();
-      setPokemon(data.results);
+    useEffect(() => {
+        async function fetchPokemon() {
+        const response = await fetch('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20');
+        const data = await response.json();
+        setPokemon(data.results);
+        }
+
+        fetchPokemon();
+    }, []);
+
+    useEffect(() => {
+        if (pokemon.length > 0) {
+        const fetchImages = async () => {
+            const images = await Promise.all(
+            pokemon.map(async (p) => {
+                const res = await fetch(p.url);
+                const data = await res.json();
+                return data.sprites.front_default;
+            })
+            );
+            setPokemonImage(images);
+        };
+
+        fetchImages();
+        }
+    }, [pokemon]);
+
+    function shuffleArray(array) {
+        const newArray = array.slice();
+        for (let i = newArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+        }
+        return newArray
     }
 
-    fetchPokemon();
-  }, []);
-
-  useEffect(() => {
-    if (pokemon.length > 0) {
-      const fetchImages = async () => {
-        const images = await Promise.all(
-          pokemon.map(async (p) => {
-            const res = await fetch(p.url);
-            const data = await res.json();
-            return data.sprites.front_default;
-          })
-        );
-        setPokemonImage(images);
-      };
-
-      fetchImages();
+    function onCardClick() {
+        const newPokemon = shuffleArray(pokemon);
+        setPokemon(newPokemon);
     }
-  }, [pokemon]);
 
-  return (
-    <div className="cards-container">
-      {pokemon.map((pokemon, index) => (
-        <Card 
-          key={index}
-          name={pokemon.name}
-          pokemonImg={pokemonImage[index]}
-        />
-      ))}
-    </div>
-  );
+    return (
+        <div className="cards-container">
+            {pokemon.map((pokemon, index) => (
+                <Card 
+                    onClick={onCardClick}
+                    key={index}
+                    name={pokemon.name}
+                    pokemonImg={pokemonImage[index]}
+                />
+            ))}
+        </div>
+    );
 }
 
 export default Cards;
