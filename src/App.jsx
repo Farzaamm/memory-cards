df0563ca12501b663805a40b77f6b3b21892cfb3
Farzaamm <93753345+Farzaamm@users.noreply.github.com>
2024-07-10 09:34:16 +0800

    score state added to manage the score

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Cards from './components/Cards';
 
 function App() {
+  const [score, setScore] = useState(0)
+
   return (
     <div className='app-component'>
-      <Header />
-      <Cards />
+      <Header score={score} />
+      <Cards score={score} setScore={setScore} />
       <Footer />
     </div>
   );
