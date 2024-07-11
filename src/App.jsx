cbdb31dadf3992be8ce47d0a93cd481a2f9ec62c
Farzaamm <93753345+Farzaamm@users.noreply.github.com>
2024-07-11 09:15:42 +0800

    reset game and GameOver added

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,29 @@ import React, { useCallback, useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Cards from './components/Cards';
+import GameOver from './components/GameOver';
 
 function App() {
   const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(false)
 
+  function resetGame() {
+    setScore(0)
+    setGameOver(false)
+  }
   return (
-    <div className='app-component'>
-      <Header score={score} />
-      <Cards score={score} setScore={setScore} />
-      <Footer />
-    </div>
+    <>
+      {gameOver && <GameOver score={score} resetGame={resetGame}/>}
+      {!gameOver && <div className='app-component'>
+        <Header score={score} />
+        <Cards 
+          score={score} 
+          setScore={setScore} 
+          setGameOverScreen={setGameOver}
+        />
+        <Footer />
+      </div>}
+    </>
   );
 }
 
